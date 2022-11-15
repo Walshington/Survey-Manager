201faca914a29ca16641982618a0903e45d42fa6
root <root@SurveyManager.oo05h2arbjmubofq3hxykj400b.bx.internal.cloudapp.net>
2022-11-15 22:03:53 +0000

    Live server now working

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//Serve the static react build
+app.use(express.static('../client/build'))
+
 //Use this if we want to serve static files
 //app.use(express.static('public'));
 
@@ -9,7 +12,7 @@ app.use(require('./routes'));  //http://127.0.0.1:8000/    http://127.0.0.1:8000
 
 //app.use("/user",require('./routes'));  //http://127.0.0.1:8000/user  http://127.0.0.1:8000/user/about
 
-var server = app.listen(3001, function () {
+var server = app.listen(80, '0.0.0.0', function () {
 
   var host = server.address().address
   var port = server.address().port
