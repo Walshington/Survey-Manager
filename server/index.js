d555172e3b07c29b5616817345ca6cecdef2c207
Walshingtonn <nicholas@bytefederal.com>
2022-11-22 14:18:46 -0500

    Database connection

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const d = require('dotenv').config();
-
 var express = require('express');
+const mysql = require('mysql')
 var app = express();
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
+const database_port = process.env.DATABASE_PORT;
+const database_name = process.env.DATABASE_NAME;
+const database_host = process.env.DATABASE_HOST;
+const database_user = process.env.DATABASE_USER;
+const database_pass = process.env.DATABASE_PASS;
 
-console.log(port);
-console.log(hostname);
 
 //Serve the static react build (Do this on live server)
 //app.use(express.static('../client/build'))
 
 //Use this if we want to serve static files
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 //Using routes from route.js file
 app.use(require('./routes'));  //http://127.0.0.1:8000/    http://127.0.0.1:8000/about
 
-//app.use("/user",require('./routes'));  //http://127.0.0.1:8000/user  http://127.0.0.1:8000/user/about
+app.use("/user",require('./routes'));  //http://127.0.0.1:8000/user  http://127.0.0.1:8000/user/about
 
 var server = app.listen(process.env.PORT, process.env.HOSTNAME, function () {
 
@@ -27,3 +30,16 @@ var server = app.listen(process.env.PORT, process.env.HOSTNAME, function () {
 
   console.log("Example app listening at http://%s:%s", host, port);
 })
+
+/* DATABASE CONNECTION */
+
+const connection = mysql.createConnection({
+  host: database_host,
+  user: database_user,
+  password: database_pass,
+  database: database_name
+})
+
+console.log(connection)
+
+connection.connect()
