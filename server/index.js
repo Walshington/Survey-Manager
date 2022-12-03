88b9ba97dfac463a65b80540458f985c417279f8
Yousef <yousef.edwards@bylight.com>
2022-12-02 19:03:44 -0500

    added config for CORS

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
-const d = require('dotenv').config();
-const mysql = require('mysql2');
-var express = require('express');
-var bodyParser = require('body-parser')
-var session = require('express-session')
+const d = require("dotenv").config();
+const mysql = require("mysql2");
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 var app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Serve the static react build (Do this on live server)
 //app.use(express.static('../client/build')
 
 //Use this if we want to serve static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Using routes from route.js file
-app.use(require('./routes'));  //http://127.0.0.1:8000/    http://127.0.0.1:8000/about
+app.use(require("./routes")); //http://127.0.0.1:8000/    http://127.0.0.1:8000/about
 
-app.use("/user",require('./routes'));  //http://127.0.0.1:8000/user  http://127.0.0.1:8000/user/about
+app.use("/user", require("./routes")); //http://127.0.0.1:8000/user  http://127.0.0.1:8000/user/about
 
 /* Server connection */
 
@@ -24,9 +32,8 @@ const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
 
 var server = app.listen(port, hostname, function () {
-
   var host = server.address().address;
   var port = server.address().port;
 
   console.log("Example app listening at http://%s:%s", host, port);
-})
+});
--- server/routes.js
+++ server/routes.js
@@ -1,63 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var sql = require('./config/db_config');
+var sql = require("./config/db_config");
 
 //Middle ware that is specific to this router (example for now)
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
+  console.log("Time: ", Date.now());
   next();
 });
 
 // Define the home page route
-router.get('/', function(req, res) {
-  res.send('home page');
+router.get("/", function (req, res) {
+  res.send("home page");
 });
 
 /*LOGIN AND REGISTER */
 
 router.post("/register", (req, res) => {
   /* Need name, email, and password sent from clientside */
+  res.set("Access-Control-Allow-Origin", "*");
   const req_name = req.body.name;
   const req_email = req.body.email;
   const req_pass = req.body.password;
-  
+
   /* If data exists */
-  if(req_name && req_email && req_pass)
-  {
-    sql.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', 
-    [req_name, req_email, req_pass],(err, rows, fields) => {
-      if (err) throw err;
-    })
-    console.log("we made it")
-    res.status(200).send('User successfully registered');
-  }
-  else
-  {
-    res.send('Please enter name, email, and password.')
+  if (req_name && req_email && req_pass) {
+    sql.query(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [req_name, req_email, req_pass],
+      (err, rows, fields) => {
+        if (err) throw err;
+      }
+    );
+    console.log("we made it");
+    res.status(200).send("User successfully registered");
+  } else {
+    res.send("Please enter name, email, and password.");
   }
 });
 
 /* I still need to add sessions to this at some point */
-router.post('/login', function(req, res) {
+router.post("/login", function (req, res) {
   /* Need email, and password sent from clientside */
   const req_email = req.body.email;
   const req_pass = req.body.password;
 
-  sql.query('SELECT * FROM users WHERE email = ? AND password = ?', 
-  [req_email, req_pass],(err, rows, fields) => {
-    if (err) throw err;
+  sql.query(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [req_email, req_pass],
+    (err, rows, fields) => {
+      if (err) throw err;
 
-    if(rows.length > 0)
-    {
-      /* Login the user */
-      res.status(200).send('User successfully logged in');
+      if (rows.length > 0) {
+        /* Login the user */
+        res.status(200).send("User successfully logged in");
+      } else {
+        res.status(201).send("user not found");
+      }
     }
-    else
-    {
-      res.status(201).send('user not found');
-    }
-  })
+  );
 });
-
 
 module.exports = router;
